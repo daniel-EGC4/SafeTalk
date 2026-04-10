96402c4404f377c962246a81bae42f57a887ceca
Daniel-EGC4 <danieleguerreroc@gmail.com>
2026-04-09 20:32:43 -0600

    Auto-commit: 2026-04-09 20:32:43

--- src/SafeTalk/SafeTalk.jsx
+++ src/SafeTalk/SafeTalk.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo.jpg"
 import React, { useState } from "react";
 import ChatApp from "../Chat/chat.jsx";
 import { useNavigate } from "react-router-dom";
-import useStyles from "../Styles/index.css";
+import useStyles from "./Estilos.jsx";
 import historiasData from "../data/historias.json";
 
 const useHistorias = () => {
