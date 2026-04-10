a6e2bfc715a1755e77f10c0016faefbcad946463
Daniel-EGC4 <danieleguerreroc@gmail.com>
2026-04-09 23:59:01 -0600

    Auto-commit: 2026-04-09 23:59:01

--- src/Historias/Exploracion-historias.jsx
+++ src/Historias/Exploracion-historias.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useStyles from "../SafeTalk/Estilos.jsx";
-import historiasData from "./data/historias.json";
-import logo from "./assets/logo.jpg";
+import historiasData from "../data/historias.json";
+import logo from "../assets/logo.jpg";
 
 // ── Estilos ──────────────────────────────────────────────────────────────────
 
